6ec2fb92f9d554cb5f621830354ca3cace0f9994
Taufik Iqbal Ramdhani <taufikiqbalr@gmail.com>
2026-09-17 20:26:03 +0700

    feat: bootstrap W1 auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
+import { AuthProvider } from './auth/AuthContext';
 import './styles.css';
+import './auth.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 );
